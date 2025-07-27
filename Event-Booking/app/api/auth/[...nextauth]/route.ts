d4dec8494684c6abc65185aa07d887014177ff25
Resham <reshamupadhyay48@gmail.com>
2025-07-27 23:06:46 +0530

    Update route.ts

--- Event-Booking/app/api/auth/[...nextauth]/route.ts
+++ Event-Booking/app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,33 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-const handler =  NextAuth({
+const handler = NextAuth({
   providers: [
-    // CredentialsProvider({
-    //   name: "Credentials",
-    //   credentials: {
-    //     username: { label: "Username", type: "text", placeholder: "jsmith" },
-    //     password: { label: "Password", type: "password" },
-    //   },
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
 
-    //   async authorize(credentials, req) {
-        
-    //   },
-    // }),
+      async authorize(credentials, req) {
+        const username = credentials?.username;
+        const password = credentials?.password;
+
+        const user = {
+          name: "user name",
+          id: 1,
+          username: "example@email.com",
+        };
+
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
   ],
 });
 
-export {handler as GET, handler as POST}
+export { handler as GET, handler as POST };
